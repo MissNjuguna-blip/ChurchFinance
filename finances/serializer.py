ddb4856fa3245cb4829f7d44409b732edf718c67
MissNjuguna-blip <vwaruinu@gmail.com>
2026-09-14 00:08:00 +0300

    corrected Member serializer, contribution serializer

--- finances/serializer.py
+++ finances/serializer.py
@@ -10,11 +10,38 @@ class ContributionTypeSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+# finances/serializer.py
+
+from rest_framework import serializers
+from core.models import Contribution
+
+
 class ContributionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contribution
-        fields = "__all__"
-        read_only_fields = ["recorded_by"]
+        fields = [
+            "id",
+            "member",
+            "contribution_type",
+            "amount",
+            "contribution_date",
+            "payment_method",
+            "recorded_by",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = [
+            "id",
+            "recorded_by",
+            "created_at",
+            "updated_at",
+        ]
+        extra_kwargs = {
+            "member": {
+                "required": False,
+                "allow_null": True,
+            },
+        }
 
 
 class ExpenseSerializer(serializers.ModelSerializer):
--- member/serializer.py
+++ member/serializer.py
@@ -1,30 +1,31 @@
 from rest_framework import serializers
-from core.models import Contribution
+from core.models import Member
 
 
-class ContributionSerializer(serializers.ModelSerializer):
+class MemberSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Contribution
+        model = Member
         fields = [
             "id",
-            "member",
-            "contribution_type",
-            "amount",
-            "contribution_date",
-            "payment_method",
-            "recorded_by",
+            "first_name",
+            "last_name",
             "created_at",
             "updated_at",
         ]
         read_only_fields = [
             "id",
-            "recorded_by",
             "created_at",
             "updated_at",
         ]
         extra_kwargs = {
-            "member": {
+            "first_name": {
                 "required": False,
                 "allow_null": True,
+                "allow_blank": True,
+            },
+            "last_name": {
+                "required": False,
+                "allow_null": True,
+                "allow_blank": True,
             },
         }
